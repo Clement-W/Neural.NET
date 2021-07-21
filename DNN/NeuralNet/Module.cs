03bf64b4bd1604d9bcf8918796d32e5093959dc5
Wclement <clement@weinreich.fr>
2021-07-21 16:31:10 +0200

    Update Readme : add todolist

--- DNN/NeuralNet/Module.cs
+++ DNN/NeuralNet/Module.cs
@@ -9,7 +9,7 @@ namespace NeuralNet
 
         // Return every properties of type Parameter or Module
         public IEnumerable<Parameter> Parameters()
-        { //TODO: Recursion is not computationnally effective, find another way to do that
+        { //TODO: Is this method efficient ? Would it be better with an array instead of using Reflection ?
 
             PropertyInfo[] properties = this.GetType().GetProperties();
             // Foreach of the properties, get the Parameter and Module ones to return every Parameter properties
